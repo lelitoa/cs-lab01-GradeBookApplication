b334543f6d03a69f792c4fcadc3c8b6e943a89f4
lelitoa <adam.lelito@microsoft.wsei.edu.pl>
2022-03-15 13:51:21 +0100

    Creating_the_RankedGradeBook_class

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-    internal class StandardGradeBook : BaseGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
         public StandardGradeBook(string name) : base(name)
         {
-            //BaseGradeBook baseGradeBook = new StandardGradeBook(name);
-
+            GradeBookType gradeBookType = GradeBookType.Standard;
         }
     }
 }
